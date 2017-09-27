4a6f126b73c94629e1ca45905470da9d86565072
Tyler Green <gree0209@stthomas.edu>
2017-09-27 15:07:45 -0500

    removed un-needed variables, changed over duplicate code to function. Added comments to execv,

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include <algorithm>
 #include <sys/types.h>
 #include <unistd.h>
-#include <dirent.h> 
+#include <dirent.h>
 
 //This is a CPP that will be compiled under c++ standard 11
 //compilable with g++ -o main main.cpp -std=c++11
@@ -12,36 +12,35 @@ using namespace std;
 
 string* splitStringWithDelimeter(string path, string delimeter);
 
-string SearchPath(string path, string *splitted); 
+string SearchPath(string path, string *splitted);
 
 int main() {
     //getenv() will determine the PATH environment variable
     //use fork() and execv() for spawning new processes
     string id;
-    bool valid = false;
     string path = getenv("PATH");
-    long amountOfColons = count(path.begin(),path.end(),':'); //10 colons  11 strings
-    string *splitted =  new string[amountOfColons+1];
-    int position = 0;
-    string delimiter = ":";
+    string *splitted;
+    string colonDelimiter = ":";
 
-    //cout << path << endl;
+    cout << path << endl;
 
-    for(int i=0; i<amountOfColons+1; i++) {
+    /*for(int i=0; i<amountOfColons+1; i++) {
         position = path.find(delimiter);
         splitted[i] = path.substr(0, position);
-        //cout << splitted[i] << endl;
+        cout << splitted[i] << endl;
         path.erase(0, position + delimiter.length());
-    }
-    
+    }*/
+
+    splitted = splitStringWithDelimeter(path,colonDelimiter); // this function is to only be used on delimeters of String type of size 1
+
     bool running = true;
-    
+
     // Input/Output to screen
     cout << "Welcome to OSShell!! Please enter your commands ('exit' to quit)." << endl;
     while(running == true)
     {
-        // cout << "\033[1;32m" <<  "Hello world"  << "\033[0m\n"; 
-        // Colors: Green(32) and Blue(34) from cplusplus.com 
+        // cout << "\033[1;32m" <<  "Hello world"  << "\033[0m\n";
+        // Colors: Green(32) and Blue(34) from cplusplus.com
         cout << "osshell> ";
         getline (cin, id);
         if(id == "exit")
@@ -49,17 +48,16 @@ int main() {
             running = false;
             break;
         }
-        
-        int spaces = count(id.begin(),id.end(),' ');
-        string *arg = new string[spaces+1]; // One less space than arguments
-        int position = 0;
-        string delimiter = " ";
-        for(int i=0; i<amountOfColons+1; i++) {
+
+        string *arg;
+        string spaceDelimiter = " ";
+        /*for(int i=0; i<amountOfColons+1; i++) {
             position = id.find(delimiter);
             arg[i] = id.substr(0, position);
             id.erase(0, position + delimiter.length());
-        }
-        
+        }*/
+        arg = splitStringWithDelimeter(id,spaceDelimiter);
+
         string pathLocation = SearchPath(arg[0],splitted);
         cout << pathLocation << endl;
         if (pathLocation != "")
@@ -75,42 +73,44 @@ int main() {
                 {
                     // Child Process
                     execv(pathLocation.c_str(),arg);
+                    //input is a string* but the function needs a char* const*
                 } break;
                 default:
                 {
                     // Parent Process
                 } break;
-            
+
             }
-            
+
         }
         else
         {
             cout << id << ": Error running command" << endl;
         }
-    }  
+    }
     return 0;
 }
 
 string* splitStringWithDelimeter(string path, string delimiter) {
     int position = 0;
-    long amountOfColons = count(path.begin(),path.end(),':');
+    long amountOfColons = count(path.begin(),path.end(),delimiter.at(0));
     string *splitted =  new string[amountOfColons+1];
-    for(int i=0; i<amountOfColons; i++) {
+    for(int i=0; i<amountOfColons+1; i++) {
         position = path.find(delimiter);
         splitted[i] = path.substr(0, position);
-        cout << splitted[i] << endl;
+        //cout << splitted[i] << endl;
         path.erase(0, position + delimiter.length());
     }
+    return splitted;
 }
 
-string SearchPath(string path, string *splitted) 
+string SearchPath(string path, string *splitted)
 // Search the path directories to see if command exists
 {
     /* Used cplusplus.com to understand file directories 
     in C++ and how to manuver in them*/
     for(int i = 0; i < 11; i++)
-    {   
+    {
         DIR *dir = opendir(splitted[i].c_str());
         if (dir == NULL)
         {
@@ -128,6 +128,6 @@ string SearchPath(string path, string *splitted)
                 }
             }
         }
-    }  
+    }
     return "";
 }
