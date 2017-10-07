e182f35e2d617dae6f59e6dd22b6e1d4f89c77e0
clem0051 <31710421+clem0051@users.noreply.github.com>
2017-10-06 21:56:02 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,10 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <dirent.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <sys/wait.h>
+#include <pwd.h>
 
 //This is a CPP that will be compiled under c++ standard 11
 //compilable with g++ -o main main.cpp -std=c++11
@@ -13,25 +17,26 @@ using namespace std;
 string* splitStringWithDelimeter(string path, string delimeter);
 
 string SearchPath(string path, string *splitted);
+void lsr(string arg);
+void lsRecursion(DIR *parent, string parentPath, int howDeep);
+void printDirent(DIR *directory, int howDeep);
+string trimWhiteSpace(string str);
+
 
 int main() {
     //getenv() will determine the PATH environment variable
     //use fork() and execv() for spawning new processes
+	//lsr("~/"); //just used for testing lsr()
+
+
     string id;
     string path = getenv("PATH");
-    string *splitted;
     string colonDelimiter = ":";
+    string *splitted = splitStringWithDelimeter(path,colonDelimiter); // this function is to only be used on delimeters of String type of size 1
 
     //cout << path << endl;
 
-    /*for(int i=0; i<amountOfColons+1; i++) {
-        position = path.find(delimiter);
-        splitted[i] = path.substr(0, position);
-        cout << splitted[i] << endl;
-        path.erase(0, position + delimiter.length());
-    }*/
 
-    splitted = splitStringWithDelimeter(path,colonDelimiter); // this function is to only be used on delimeters of String type of size 1
 
     bool running = true;
 
@@ -48,48 +53,60 @@ int main() {
             running = false;
             break;
         }
-        string spaceDelimiter = " ";
-        /*for(int i=0; i<amountOfColons+1; i++) {
-            position = id.find(delimiter);
-            arg[i] = id.substr(0, position);
-            id.erase(0, position + delimiter.length());
-        }*/
-        string *temp = splitStringWithDelimeter(id,spaceDelimiter);
-        int len = count(id.begin(),id.end(),' ');
-        char **arg = new char*[len + 1];
-        for(int i = 0; i < len+1; i++)
-        {
-            arg[i] = (char*)temp[i].c_str();
-        }
 
-        string pathLocation = SearchPath(arg[0],splitted);
-        //cout << pathLocation << endl;
-        if (pathLocation != "")
-        {
-            pid_t pFork = fork();
-            switch(pFork)
-            {
-                case -1:
-                {
-                    // Error Handling
-                } break;
-                case 0:
-                {
-                    // Child Process
-                    execv(pathLocation.c_str(),arg);
-                    //input is a string* but the function needs a char* const*
-                } break;
-                default:
-                {
-                    // Parent Process
-                } break;
+        if(id != "") {
+            //Looked for best way to trim whitespace
+            //https://stackoverflow.com/questions/25829143/c-trim-whitespace-from-a-string
+            if (trimWhiteSpace(id) == "lsr") {
+                lsr("");
+            } else if (trimWhiteSpace(id).substr(0,4) == "lsr ") {
+                lsr(trimWhiteSpace(id.substr(3)));
+            } else {
+                string spaceDelimiter = " ";
+                string *temp = splitStringWithDelimeter(id, spaceDelimiter);
+                int returnStatus;
 
+                int len = count(id.begin(), id.end(), ' ');
+                char **arg = new char *[len + 1];
+                for (int i = 0; i < len + 1; i++) {
+                    arg[i] = (char *) temp[i].c_str();
+                }
+                arg[len + 1] = NULL;
+                string pathLocation = SearchPath(arg[0], splitted);
+                //cout << "The given arguments are: " << endl;
+                /*for(int i=0; i<len+1; i++) {
+                    cout << arg[i] << endl;
+                }*/
+                //cout << "The found path is: " << pathLocation << endl;
+                if (pathLocation != "") {
+                    pid_t pFork = fork();
+                    switch (pFork) {
+                        case -1: {
+                            // Error Handling
+                            //cout << "ERROR FORK" << endl;
+                        }
+                            break;
+                        case 0: {
+                            // Child Process
+                            //cout << "Child Process " << endl;
+                            execv(pathLocation.c_str(), arg);
+                        }
+                            break;
+                        default: {
+                            // Parent Process
+                            //cout << "PARENT PROCESS" << endl;
+                            //wait(NULL);
+                            waitpid(pFork, &returnStatus, 0);
+                            //cout << "PARENT DONE WAITING" << endl;
+                        }
+                            break;
+
+                    }
+
+                } else {
+                    cout << id << ": Error running command" << endl;
+                }
             }
-
-        }
-        else
-        {
-            cout << id << ": Error running command" << endl;
         }
     }
     return 0;
@@ -135,3 +152,123 @@ string SearchPath(string path, string *splitted)
     }
     return "";
 }
+
+string trimWhiteSpace(string str) {
+    size_t first = str.find_first_not_of(' ');
+    if (string::npos == first)
+    {
+        return str;
+    }
+    size_t last = str.find_last_not_of(' ');
+    return str.substr(first, (last - first + 1));
+}
+
+void lsr(string arg)
+/*TODO:
+	-implement printDir
+	-error checking
+*/
+
+//if they type in an argument, pass that. If not, pass an empty string
+{
+	
+	if(arg.compare("")==0)
+	{
+		//printf("<NO ARG GIVEN>\n");//debug text
+		DIR *dir = opendir("./");
+		lsRecursion(dir,"./", 0);
+		
+	} else {
+		//printf("<ARG GIVEN>\n"); //debug text
+
+		if(!arg.compare(0,1,"~"))
+		{//if using shorthand for home folder
+			arg.replace(0,1,getpwuid(getuid())->pw_dir);
+		}
+
+		DIR *dir = opendir(arg.c_str());
+
+		//input checking
+		if( dir == NULL)
+		{//if unable to open file
+			perror("Unable to open");
+		}
+		else
+		{
+			DIR *dir = opendir(arg.c_str());
+			lsRecursion(dir,arg, 0);
+		}
+		
+	}
+	
+	
+}
+
+void lsRecursion(DIR *parent,string parentPath, int howDeep)
+
+{
+	//printf("lsRecursion started\n");
+	struct stat s;
+	dirent *currentEntry;
+	
+	//printDirent(parent, howDeep); //MOVED TO NEW LOCATION, SEE BELOW
+	
+	while(( currentEntry = readdir(parent)) != NULL)
+	{
+		//printf("howDeep= %d",howDeep);
+		if(currentEntry->d_name[0]!='.' )
+		{   
+		    //if the name of the entry doesn't start with a period
+			//printf("FILE WITH NO PERIOD: ");
+			string f = "";
+            for(int i=0; i<howDeep; i++)
+            {
+                f += "    ";
+            }
+		    //(if it starts with a period, we can ignore it)	
+			if(stat(currentEntry->d_name, &s) == 0 && S_ISDIR(s.st_mode))
+			{//if it's a directory
+			    cout << f << "\033[1;34m" << currentEntry->d_name << " (Directory)\033[0m\n";
+			    DIR *recursiveDir = opendir( (parentPath + currentEntry->d_name).c_str() );
+				lsRecursion(recursiveDir,parentPath + currentEntry->d_name, howDeep+1);
+			}
+			else if(s.st_mode & S_IEXEC)
+			{
+			    cout << f << "\033[1;32m" << currentEntry->d_name << " (" << s.st_size << " bytes)"<< "\n";
+			}
+			else
+			{
+			    cout << f << "\033[0m" << currentEntry->d_name << " (" << s.st_size << " bytes)"<< "\n";
+			}
+			cout << "\033[0m";
+		} else {
+			//printf("FILE WITH A PERIOD: ");
+			//cout << currentEntry->d_name << "\n";
+		}
+	}
+}
+
+/*
+void printDirent(DIR *directory, int howDeep) //The below is an idea or start for printing things out from the directory 
+{
+	if ((data.attribute & _A_SUBDIR) == _A_SUBDIR)
+	{
+		cout <<	"[" <<data.name<< "]" << endl;
+	}
+	else
+	{
+		cout << data.name << endl;
+	}
+	
+}
+logic for printDir:
+printdir(dir,howdeep)
+string prefix = "";
+for(int i=0; i<howdeep; i++)
+{
+	prefix += "\t";
+}
+<printing logic>
+print line:
+cout << (color) << prefix << filename << "\n" << (default color);
+	*/
